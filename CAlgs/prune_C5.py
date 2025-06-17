1fe81ff32fb71a121b2e108962ab7df6151360be
shoki-hihara <131498289+shoki-hihara@users.noreply.github.com>
2025-06-17 15:17:59 +0900

    Update prune_C5.py

--- CAlgs/prune_C5.py
+++ CAlgs/prune_C5.py
@@ -12,7 +12,7 @@ import torch.nn.functional as F
 from utils import *
 from train import *
 import copy
-
+import time
 
 class HOS:
     def __init__(self, data, save_dir, arch, epochs_step3=250, epochs_step4=50, rate=0.7, metric='k34', strategy='P2', mse_factor=1, kd_params=(None, None), fixed_seed=False, use_logger=True):
@@ -20,7 +20,7 @@ class HOS:
         self.data_dir = data['dir']
         self.data_name = data['name']
         self.save_dir = save_dir
-        self.arch = arch['dir']
+        self.arch = arch['/content/drive/MyDrive/学習/大学院/特別研究/AutoMC/model_cifar100_vgg16_best.pth.tar']
         self.arch_name = arch['name']
         self.rate = rate
         self.rate1 = math.sqrt(math.sqrt(rate))
@@ -58,7 +58,6 @@ class HOS:
             self.logger.info("The calculated coef is {}".format(coef))
             self.logger.info("The actual pruning rate is {}".format(rate))
 
-        # Decide how many and which filters to prune at each layer
         compressed_channels = 0
         total_channels = 0
         for index in range(len(modules)):
@@ -78,7 +77,6 @@ class HOS:
                     m.weight.data[i].mul_(mask[i])
                 cfg_mask.append(mask.clone())
                 cfg.append(num_keep)
-                # self.logger.info('layer index: {:d} \t total channel: {:d} \t remaining channel: {:d}'.format(index, mask.shape[0], num_keep))
                 compressed_channels += num_keep
                 total_channels += mask.shape[0]
         if self.logger:
@@ -146,10 +144,8 @@ class HOS:
         optimizer = optim.SGD(model.parameters(), momentum=0.9, lr=self.lr_step3,  weight_decay=1e-4)
         lr_scheduler = get_lr_scheduler(self.lr_sche, optimizer, self.epochs_step3)
 
-        # Extract the softmax layer values from the baseline network
         output = test_orig_for_step3(model_init, train_loader, self.data_name, self.logger)
 
-        # Test before training
         if self.logger:
             self.logger.info("Model:")
         test_at_beginning(model, val_loader, self.logger)
@@ -160,37 +156,30 @@ class HOS:
         best_acc_top1 = 0.0
         timer = train_timer()
 
-        # Get filename
         filename, bestname = get_filename_training(self.save_dir, 'step3')
 
         for epoch in range(self.epochs_step3):
-            # Run one epoch
             need_time = timer.get_need_time(self.epochs_step3, epoch)
             if self.logger:
                 self.logger.info("Epoch {}/{}  {:s}  lr={}".format(epoch + 1,
                                  self.epochs_step3, need_time, optimizer.param_groups[0]['lr']))
 
-            # Train for one epoch
             train_for_step3(model, optimizer, train_loader, output, self.mse_factor, self.logger)
 
             lr_scheduler.step()
 
-            # Evaluate on validation set
             val_metrics = validate(model, val_loader, self.logger)
             val_acc_top1 = val_metrics['acc_top1']
             is_best = val_acc_top1 >= best_acc_top1
 
-            # Save weights
             save_checkpoint(model, is_best, filename, bestname)
             if is_best:
                 if self.logger:
                     self.logger.info("- Found new best accuracy on validation set")
                 best_acc_top1 = val_acc_top1
-                # Save best val metrics in a json file in the model directory
                 best_json_path = os.path.join(self.save_dir, "metrics_val_best_weights.json")
                 save_dict_to_json(val_metrics, best_json_path)
 
-            # Save latest val metrics in a json file in the model directory
             last_json_path = os.path.join(self.save_dir, "metrics_val_last_weights.json")
             save_dict_to_json(val_metrics, last_json_path)
 
@@ -202,24 +191,23 @@ class HOS:
             self.logger.info(">>>>>> Starting C5")
             self.logger.info("Target rate is: {}".format(self.rate))
 
-        # Load original model
-        model = torch.load(self.arch)
+        device = torch.device("cuda" if self.cuda else "cpu")
+        model = torch.load(self.arch, map_location=device)
         if self.logger:
             self.logger.info("loaded model '{}' from {}".format(self.arch_name, self.arch))
             self.logger.info("The original model's cfg={}".format(model.cfg))
 
         if self.cuda:
             model = model.cuda()
-        
-        # Load data
+        else:
+            model = model.to('cpu')
+
         train_loader, val_loader = models.load_data(self.data_name, self.data_dir, arch_name=self.arch_name)
         if self.logger:
             self.logger.info("Loaded dateset '{}' from '{}'".format(self.data_name, self.data_dir))
 
-        # Test before compressing
         metrics_original = test_at_beginning(model, val_loader, self.logger)
 
-        # Step1
         model_step1 = self.step1(copy.deepcopy(model))
         compression_rate = get_compression_rate(model, model_step1)
         if self.logger:
@@ -227,7 +215,6 @@ class HOS:
             self.logger.info('After step1, The compression rate is {}'.format(compression_rate))
         test_at_beginning(model_step1, val_loader, self.logger, message='After step1, accu is')
 
-        # Step2
         self.get_rate(compression_rate)
         model_step2 = self.step2(copy.deepcopy(model_step1))
         compression_rate = get_compression_rate(model, model_step2)
@@ -235,16 +222,13 @@ class HOS:
             self.logger.info("After step2, the model's arch is {}".format(model_step2))
             self.logger.info('After step2, The compression rate is {}'.format(compression_rate))
 
-        # Step3
         model_step3 = self.step3(copy.deepcopy(model_step2), model, train_loader, val_loader)
 
-        # Step4
         if self.logger:
             self.logger.info(">>> Step4")
         model_step4, val_metrics = fine_tune(self.save_dir, copy.deepcopy(model_step3), train_loader, val_loader,
                                              epochs=self.epochs_step4, lr=self.lr_step4, lr_sche=self.lr_sche, logger=self.logger, kd_params=self.kd_params, return_file=True, use_logger=self.use_logger).main()
 
-        # Calculate metrics
         result = calc_result(model, metrics_original, torch.load(model_step4), val_metrics, model_step4, self.logger)
         save_result_to_json(self.save_dir, result)
 
@@ -257,7 +241,7 @@ if __name__ == '__main__':
     arch_name = 'vgg13'
     data = {'dir': './data', 'name': 'mini_cifar10'}
     save_dir = './snapshots/{}/C5/'.format(arch_name)
-    arch = {'dir': './trained_models/{}/{}.pth.tar'.format(data['name'], arch_name), 'name': arch_name}
+    arch = {'dir': '/content/drive/MyDrive/trained_models/{}/{}.pth.tar'.format(data['name'], arch_name), 'name': arch_name}
     h = HOS(data, save_dir, arch, metric='k34', strategy='P2', fixed_seed=True, epochs_step3=1, epochs_step4=1)
     print(h.main())
 '''
